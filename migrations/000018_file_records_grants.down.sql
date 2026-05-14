0000dda5b9c7071ca7b71abaee7e8f2d6426052c
manxiu-dev <manxiu-dev@wuhumanxiu.com>
2026-05-14 11:32:43 +0800

    fix(migrations): guard file record grants by role

--- migrations/000018_file_records_grants.down.sql
+++ migrations/000018_file_records_grants.down.sql
@@ -1,2 +1,7 @@
-REVOKE SELECT, INSERT, UPDATE, DELETE ON TABLE file_records FROM agent_im;
-REVOKE USAGE, SELECT ON SEQUENCE file_records_id_seq FROM agent_im;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'agent_im') THEN
+    REVOKE SELECT, INSERT, UPDATE, DELETE ON TABLE file_records FROM agent_im;
+    REVOKE USAGE, SELECT ON SEQUENCE file_records_id_seq FROM agent_im;
+  END IF;
+END $$;
--- migrations/000018_file_records_grants.up.sql
+++ migrations/000018_file_records_grants.up.sql
@@ -1,5 +1,10 @@
-ALTER TABLE IF EXISTS file_records OWNER TO agent_im;
-ALTER SEQUENCE IF EXISTS file_records_id_seq OWNER TO agent_im;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'agent_im') THEN
+    ALTER TABLE IF EXISTS file_records OWNER TO agent_im;
+    ALTER SEQUENCE IF EXISTS file_records_id_seq OWNER TO agent_im;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE file_records TO agent_im;
-GRANT USAGE, SELECT ON SEQUENCE file_records_id_seq TO agent_im;
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE file_records TO agent_im;
+    GRANT USAGE, SELECT ON SEQUENCE file_records_id_seq TO agent_im;
+  END IF;
+END $$;
